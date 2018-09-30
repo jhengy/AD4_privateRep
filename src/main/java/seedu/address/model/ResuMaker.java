fa81977262e262d2a4b4d6b549dce3937b6a9332
jhengy <35480886+jhengy@users.noreply.github.com>
2018-09-30 21:25:48 +0800

    initialize

---
+++ src/main/java/seedu/address/model/ResuMaker.java
@@ -0,0 +1,9 @@
+package seedu.address.model;
+
+/**
+ * Wraps all data at the ResuMaker level
+ * Duplicates are not allowed (by .isSameEntry comparison)
+ */
+public class ResuMaker {
+
+}
---
+++ src/main/java/seedu/address/storage/XmlFileStorage.java
@@ -0,0 +1,40 @@
+package seedu.address.storage;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+
+import javax.xml.bind.JAXBException;
+
+import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.commons.util.XmlUtil;
+
+/**
+ * contains static methods that Stores addressbook data in an XML file
+ * i.e. conversion between XmlSerializableAddressBook to file and vice versa
+ */
+public class XmlFileStorage {
+    /**
+     * Saves the given addressbook data to the specified file.
+     */
+    public static void saveDataToFile(Path file, XmlSerializableAddressBook addressBook)
+            throws FileNotFoundException {
+        try {
+            XmlUtil.saveDataToFile(file, addressBook);
+        } catch (JAXBException e) {
+            throw new AssertionError("Unexpected exception " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Returns address book in the file or an empty address book
+     */
+    public static XmlSerializableAddressBook loadDataFromSaveFile(Path file) throws DataConversionException,
+                                                                            FileNotFoundException {
+        try {
+            return XmlUtil.getDataFromFile(file, XmlSerializableAddressBook.class);
+        } catch (JAXBException e) {
+            throw new DataConversionException(e);
+        }
+    }
+
+}
